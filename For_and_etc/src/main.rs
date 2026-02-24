99eaccff4b1bb21f0d10ad3902405867e4739d2b
applecity01 <azamat01region.77@gmail.com>
2026-02-24 11:56:11 +0300

    Initial commit: all rust lessons

---
+++ For_and_etc/src/main.rs
@@ -0,0 +1,21 @@
+
+fn main() {
+for  number in 1..=10{
+println!("{}",number*5)
+}
+println!("Таблица умножения \n ---------------");
+let  y = 1025 ;
+let mut f = 0;
+let  mut  t = y ;
+if  y % 2 != 0 {
+    println!("не степень двойки");
+   return; 
+}
+
+while t > 1 {
+    t = t/2;
+  f= f + 1 ;
+    print!("{} \n",t)
+}
+println!("это {} степень двойки",f)
+}
---
+++ Operations/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a = 100;
+    let mut b = 45;
+    b = a - b;
+    let x = 100 /2 ;
+    let r = x % 2;
+    println!("{}", r + b);
+    println!("{}",b);
+}
